1c290926f3fd004e43fb07fe918944e2ed68b2c1
Hazem Ben Amor <69153224+Hazoumaba@users.noreply.github.com>
2024-06-15 21:56:35 -0400

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <footer
+      className="bg-neutral-100 text-center text-gray-900 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+      <div
+        className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
+        <div className="mr-12 hidden lg:block">
+          <span>Rejoignez-nous sur les réseaux sociaux :</span>
+        </div>
+        {/* <!-- Social network icons container --> */}
+        <div className="flex justify-center">
+          <a className="mr-6 text-gray-900 dark:text-neutral-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path
+                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+            </svg>
+          </a>
+          <a className="mr-6 text-gray-900 dark:text-neutral-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path
+                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+            </svg>
+          </a>
+          <a className="mr-6 text-gray-900 dark:text-neutral-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path
+                d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+            </svg>
+          </a>
+
+        </div>
+      </div>
+
+      {/* <!-- Main container div: holds the entire content of the footer, including four sections (TW Elements, Products, Useful links, and Contact), with responsive styling and appropriate padding/margins. --> */}
+      <div className="mx-8 py-5 text-center md:text-left">
+        <div className="grid-2 grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+          {/* <!-- TW Elements section --> */}
+          <div className="">
+            <h6
+              className="mb-4 flex items-center justify-center font-semibold md:justify-start">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 24 24">
+                <path
+                  d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.848 12.459c.202.038.202.333.001.372-1.907.361-6.045 1.111-6.547 1.111-.719 0-1.301-.582-1.301-1.301 0-.512.77-5.447 1.125-7.445.034-.192.312-.181.343.014l.985 6.238 5.394 1.011z" />
+              </svg>
+              Service clientèle
+            </h6>
+            <p>
+              Lundi au Vendredi : 8h00 - 20h00
+            </p>
+            <p>
+              Samedi : 9h00 - 17h00
+            </p>
+            <p>
+              Dimanche : 10h00 - 14h00
+            </p>
+
+          </div>
+          {/* <!-- Products section --> */}
+          {/* <!-- Useful links section --> */}
+          <div className="">
+            <h6
+              className="mb-4 flex justify-center font-semibold  md:justify-start">
+              <svg width="30" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                <path
+                  d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
+              </svg>
+              Lien utiles
+            </h6>
+            <p className="mb-4">
+              <a className="text-gray-900 dark:text-neutral-200"
+              >Lorem</a>
+            </p>
+            <p className="mb-4">
+              <a className="text-gray-900 dark:text-neutral-200"
+              >Lorem</a>
+            </p>
+            <p className="mb-4">
+              <a className="text-gray-900 dark:text-neutral-200"
+              >Lorem</a>
+            </p>
+          </div>
+          {/* <!-- Contact section --> */}
+          <div>
+            <h6
+              className="mb-4 flex justify-center font-semibold  md:justify-start">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 24 24">
+                <path
+                  d="M23 0l-4.5 16.5-6.097-5.43 5.852-6.175-7.844 5.421-5.411-1.316 18-9zm-11 12.501v5.499l2.193-3.323-2.193-2.176zm-8.698 6.825l-1.439-.507 5.701-5.215 1.436.396-5.698 5.326zm3.262 4.287l-1.323-.565 4.439-4.503 1.32.455-4.436 4.613zm-4.083.387l-1.481-.507 8-7.89 1.437.397-7.956 8z" />
+              </svg>
+              Contact
+            </h6>
+            <p className="mb-4 flex items-center text-gray-900 justify-center md:justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                <path
+                  d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+              </svg>
+              Adresse
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                <path
+                  d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+              </svg>
+              Email
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mr-3 h-5 w-5">
+                <path
+                  fillRule="evenodd"
+                  d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                  clipRule="evenodd" />
+              </svg>
+              Numéro de téléphone
+            </p>
+
+          </div>
+        </div>
+      </div>
+
+      {/* <!--Copyright section--> */}
+
+    </footer>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import About from '../components/About';
+import Features from '../components/Features';
+import Header from '../components/Header';
+import LazyShow from '../components/LazyShow';
+import MainHero from '../components/MainHero';
+import MainHeroImage from '../components/MainHeroImage';
+import ProgressSlider from '../components/progress-slider';
+import Product from '../components/Product';
+import SilderImg01 from '/public/ps-image-01.jpg'
+import SilderIcon01 from '/public/ps-icon-01.svg'
+import SilderIcon02 from '/public/ps-icon-02.svg'
+import SilderIcon03 from '/public/ps-icon-03.svg'
+import SilderIcon04 from '/public/ps-icon-04.svg'
+const items = [
+  {
+    img: SilderImg01,
+    desc: 'Totem Acoustic',
+    buttonIcon: SilderIcon01,
+  },
+  {
+    img: SilderImg01,
+    desc: 'Cacao 70',
+    buttonIcon: SilderIcon02,
+  },
+  {
+    img: SilderImg01,
+    desc: 'A&G Promotion',
+    buttonIcon: SilderIcon03,
+  },
+  {
+    img: SilderImg01,
+    desc: 'Milan Pole Dance Studio',
+    buttonIcon: SilderIcon04,
+  },
+]
+
+
+
+const App = () => {
+  return (
+    <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+      <div className={`relative bg-background`}>
+        <div className="max-w-7xl mx-auto">
+          <div
+            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+          >
+            <Header />
+            <MainHero />
+          </div>
+        </div>
+        <MainHeroImage />
+      </div>
+      <LazyShow>
+        <Product />
+      </LazyShow>
+      <LazyShow>
+        <ProgressSlider items={items}/>
+      </LazyShow>
+
+      <LazyShow>
+        <Features />
+      </LazyShow>
+      <LazyShow>
+          <About />
+      </LazyShow>
+
+    </div>
+  );
+};
+
+export default App;
